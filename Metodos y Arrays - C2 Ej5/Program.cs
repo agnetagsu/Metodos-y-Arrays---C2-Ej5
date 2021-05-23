f67d552fba99cdc34e13fc9a8a3e0d3ba368a90e
Agneta <agnetagsu@yahoo.es>
2021-05-23 11:14:41 +0200

    Agregar archivos de proyecto.

---
+++ Metodos y Arrays - C2 Ej5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Metodos_y_Arrays___C2_Ej5
+{
+    class PasarABinario
+    {
+        public String SuBinario(int midecimal)
+        {
+            string mibinario = "";
+            int cociente = midecimal;
+            int resultado = midecimal;
+            int resto = 0;
+
+            do
+            {
+                resultado = cociente / 2;
+                resto = cociente % 2;
+                if (resto == 0)
+                {
+                    mibinario = "0" + mibinario;
+                }
+                else
+                {
+                    mibinario = "1" + mibinario;
+                }
+                cociente = resultado;
+            }
+            while (resultado > 1);
+            mibinario = "1" + mibinario;
+            return mibinario;
+
+        }
+        static void Main(string[] args)
+        {
+            int numero;
+            PasarABinario elbinario = new PasarABinario();
+            Console.WriteLine("Introduce un numero para calcular su binario:");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(elbinario.SuBinario(numero));
+
+        }
+    }
+}
